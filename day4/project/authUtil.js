fa36707854819e132b2ba6078e2693243ad079b1
Iamneo <techadmin@iamneo.ai>
2023-12-07 11:46:46 +0000

    Auto-save-commit-12/7/2023--5:16:49-PM

--- day4/project/authUtil.js
+++ day4/project/authUtil.js
@@ -1,14 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+  const secretKey = 'nodesecret'; // Replace this with your actual secret key
+  return jwt.sign({ userId }, secretKey, {
     expiresIn: '1h', // You can adjust the expiration time as needed
   });
 };
 
 const verifyToken = (token) => {
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const secretKey = 'nodesecret'; // Replace this with your actual secret key
+    const decoded = jwt.verify(token, secretKey);
     return decoded;
   } catch (error) {
     return null; // Token is not valid
